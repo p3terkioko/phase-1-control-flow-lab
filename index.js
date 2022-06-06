574f878c690667feb6795689ab9a4aed80be6ec0
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-06 20:35:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet<= 400){
+    return 'This one is on me!'
+  }
+  else if(!feet > 2000 || feet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(feet > 2500){
+    return 'No can do.';
+  }
+}
+ 
+
+function ternaryCheckCity(city){
+  city === 'NYC'? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return 'Thank you so much.'
+  }else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }else{
+    return 'Bye.'
+  }
 }
