b868467f6f8e1b4599df9d4a58741e1c26215672
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-25 10:41:34 +0300

    commit tree 15f638907c44a9a2a5e3de5b5883c1632ed5db69

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function scuberGreetingForFeet(feet){
-  if(feet<= 400){
-    return 'This one is on me!'
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do.";
   }
-  else if(!feet > 2000 || feet <= 2500){
-    return 'I will gladly take your thirty bucks.'
-  }
-  else if(feet > 2500){
-    return 'No can do.';
-  }
+
+  
 }
- 
 
 function ternaryCheckCity(city){
-  city === 'NYC'? 'Ok, sounds good.' : 'No go.'
-
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(tip){
-  if(tip === 'generous'){
-    return 'Thank you so much.'
-  }else if(tip === 'not as generous'){
-    return 'Thank you.'
-  }else{
-    return 'Bye.'
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 }
